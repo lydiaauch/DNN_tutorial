de6e34d89dbc998c586de69281eafe1d690fb71d
auchly1 <lydia.auch@novartis.com>
2019-02-26 22:35:42 -0800

    added imports

---
+++ tutorial.py
@@ -0,0 +1,7 @@
+from sklearn import cross_validation, metrics, preprocessing
+import tensorflow as tf
+from tensorflow import learn
+import pandas as pd
+
+from sklearn.datasets import load_boston
+boston = load_boston()
